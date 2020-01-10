445382338471f7955afa8f4e4332f63748a01163
CarlKarlQarl <karlmatthes@gmail.com>
2020-01-10 11:19:04 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form")
+  const taskUL = document.getElementById("tasks")
+  const subButton = document.getElementById("button")
+
+  taskForm.addEventListener("submit", (event) => {
+    console.log("Form event", event)
+    event.preventDefault()
+    const newTaskLI = document.createElement("li")
+    newTaskLI.innerText = event.target.childNodes[3].value
+    taskUL.appendChild(newTaskLI)
+  })
+
+  subButton.addEventListener("click", (event) => {
+    console.log("Button event", event)
+  })
 });

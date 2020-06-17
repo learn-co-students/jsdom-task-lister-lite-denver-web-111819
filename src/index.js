f5930933c7eddd8a11bb09cbae08c28c0cae50fc
jcouper <joshcouper@gmail.com>
2020-06-17 15:16:25 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", function(event) {
     event.preventDefault()
     let input = document.getElementById("new-task-description")
+    let selection = document.getElementById('urgency')
     
     let li = document.createElement('li')
     let button = document.createElement('button')
+    
+    if (selection.value == 'low') {
+      li.style.color = 'green'
+    } else if (selection.value == 'medium'){
+      li.style.color = 'yellow'
+    } else if (selection.value == 'high') {
+      li.style.color = 'red'
+    }
+    
     li.innerText = input.value 
     button.innerText = "Delete Task"
     listElement.append(li)
